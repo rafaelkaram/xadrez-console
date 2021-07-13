297cd37be9d1112cfd755cabce006ecf7a92ae5e
Rafael Karam <rafael.karam10@gmail.com>
2021-07-13 11:24:18 -0300

    Jogada especial: promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -121,6 +121,19 @@ namespace xadrez {
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if (p is Peao) {
+                if((p.cor == Cor.Branca && destino.linha == 0)||(p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
@@ -136,7 +149,7 @@ namespace xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //#jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)) {//se o peao movimentou 2 casas na primeira jogada
